4e49034e1d553447d510a0c23e6a7d47ac74b5e1
rifat3790 <mdrifayethossen@gmail.com>
2024-08-19 09:23:53 +0600

    added score and life event

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -9,6 +9,54 @@
 //     playGround.classList.remove('hidden');
 // }
 
+let newScore = 0;
+// Keyup event listener
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    
+
+    // get the expected letter
+    const currentAlphabetElement = document.getElementById('current-alphabet').innerText;
+    console.log('Expected letter:', currentAlphabetElement);
+
+    // check if the player pressed the correct letter
+
+    if(playerPressed === currentAlphabetElement){
+        // correct letter pressed
+        console.log('Correct letter pressed!');
+
+        // update crore
+
+        const scoreElement = getTextContentById('score');
+        let newScore = scoreElement + 1;
+        setTextContentById('score', newScore);
+
+
+        removeBackgroundColorById(currentAlphabetElement);
+        continueGame();
+        
+    } else {
+        // incorrect letter pressed
+        console.log('Incorrect letter pressed!');
+
+        // update life
+        const lifeElementValue = getTextContentById('life');
+        const lifeElement = lifeElementValue - 1;
+        setTextContentById('life', lifeElement);
+        if(lifeElement === 0){
+            // game over
+            alert('Game Over!');
+            gameOver();
+            
+        }
+
+        // display an alert
+        alert('Incorrect letter! Try again');
+    }
+}
+
+document.addEventListener('keypress', handleKeyboardButtonPress);
+
 function continueGame(){
     // step-1: generate a random letter
     const alphabet = getARandomLetter();
@@ -24,7 +72,16 @@ function continueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
     continueGame();
     
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    setTextContentById('total-score', newScore)
+    removeBackgroundColorById(currentAlphabetElement);
+    setTextContentById('life', 3);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -18,7 +18,27 @@ function addBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
 
+// get text content by id
+
+function getTextContentById(elementId){
+    const element = document.getElementById(elementId);
+
+    const textContentInt = parseInt(element.textContent);
+    return textContentInt;
+}
+
+// set text content by id
+
+function setTextContentById(elementId, value){
+    const element = document.getElementById(elementId);
+
+    element.textContent = value;
+}
 
 function getARandomLetter(){
     // const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
